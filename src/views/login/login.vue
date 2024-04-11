300d227b14a96be05e1a007825ab48f64124d69b
wutianhang <mr_wu163@163.com>
2024-04-11 16:58:57 +0800

    测试

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,8 +1,32 @@
 <template>
-  <el-button>默认按钮</el-button>
-  <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
+<!--  <el-button>默认按钮</el-button>-->
+<!--  <el-button type="primary">主要按钮</el-button>-->
+<!--  <el-button type="success">成功按钮</el-button>-->
+<!--  <el-button type="info">信息按钮</el-button>-->
+
+  <div>
+    <el-form
+        style="max-width: 400px;"
+        status-icon
+        label-width="auto"
+        class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="name">
+        <el-input
+            v-model="aa"
+            autocomplete="off"
+            style="width: 300px"/>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+            v-model="aa"
+            type="password"
+            autocomplete="off"
+            style="width: 300px"/>
+      </el-form-item>
+
+    </el-form>
+  </div>
 
 </template>
 
@@ -15,7 +39,10 @@ export default {}
 </script>
 
 <style>
-#app {
+.demo-ruleForm {
+  justify-content: center;
+  margin: 15% auto;
+  /*border: 1px red solid;*/
 
 }
 </style>
