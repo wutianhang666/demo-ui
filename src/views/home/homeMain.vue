0cc6a19231841c356913cc1a9014bc9582c6ed72
wutianhang <mr_wu163@163.com>
2024-04-17 23:51:23 +0800

    首页布局

--- src/views/home/homeMain.vue
+++ src/views/home/homeMain.vue
@@ -1,5 +1,25 @@
 <template>
-  <h2>home主页面</h2>
+  <div style="
+    width: 99.9%;
+    height: 83vh;
+    border: 1px red solid;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;"
+  >
+    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
+
+    </div>
+    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
+
+    </div>
+    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
+
+    </div>
+    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
+
+    </div>
+  </div>
 
 </template>
 
