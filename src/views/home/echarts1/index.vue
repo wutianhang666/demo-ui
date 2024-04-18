bc96d572ab49c4242ff74bc1d9d0c29071e0c313
wutianhang <mr_wu163@163.com>
2024-04-18 15:14:55 +0800

    首页添加echars报表

---
+++ src/views/home/echarts1/index.vue
@@ -0,0 +1,132 @@
+<template>
+  <div :id="chartsId" class="echarts-container">
+
+  </div>
+</template>
+
+
+<script>
+import {onMounted, reactive, toRefs} from 'vue'
+import * as echarts from 'echarts';
+
+export default {
+  name: "echarts1",
+  props: {
+    chartsId: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+
+    const state = reactive({
+      count: 1,
+    })
+
+    const renderChart = () => {
+
+      const myChart = echarts.init(document.getElementById(props.chartsId))
+      //父组件给子组件传的值
+      console.log("props.chartsId", props.chartsId)
+
+      const option = {
+        title: {
+          text: '收入周报',
+          left: 'center'
+        },
+        //设置图表中显示数据项的详细信息
+        tooltip: {
+          show: true,
+          trigger: 'axis',
+          // axisPointer: {
+          //   type: 'shadow'
+          // }
+        },
+        //设置报上上方的标志位
+        legend: {
+          right: '20',
+          top: 10,
+          textStyle: {
+            color: 'black',
+            fontSize: '12px',
+            fontWeight: 400,
+          }
+        },
+        xAxis: {
+          data: ['1号', '2号', '3号', '4号', '5号', '6号'],
+          type: 'category',//类别型
+          axisLabel: {
+            color: 'black',
+            textStyle: {
+              fontSize: 12,
+            },
+          },
+        },
+        yAxis: {
+          type: 'value',//数值型
+          axisLabel: {
+            color: 'black',
+            textStyle: {
+              fontSize: 12,
+            },
+          },
+        },
+        //整体位置设置
+        grid: {
+          left: '5%',
+          right: '5%',
+          bottom: '4%',
+          containLabel: true,//包含标签
+        },
+        series: [
+          {
+            name: '洗车',
+            type: 'bar',//bar代表柱状图
+            data: [5, 20, 36, 10, 10, 20],
+            label: {
+              show: true,
+              //每个系列的标签显示，如果值为0，则不显示标签
+              formatter: function (val) {
+                if (val.value === 0) {
+                  return '';
+                }
+              },
+            },
+          },
+          {
+            name: '装饰',
+            type: 'bar',
+            data: [50, 20, 36, 70, 19, 70],
+            label: {
+              show: true,
+              formatter: function (val) {
+                if (val.value === 0) {
+                  return '';
+                }
+              },
+            },
+          }
+        ]
+      }
+
+      myChart.setOption(option);
+    }
+
+    onMounted(() => {
+      renderChart() // 生命周期挂载函数渲染图表
+    })
+
+    return {
+      ...toRefs(state),
+    }
+  }
+}
+</script>
+
+<style scoped>
+.echarts-container {
+  width: 99%;
+  height: 40vh;
+}
+
+</style>
--- src/views/home/homeMain.vue
+++ src/views/home/homeMain.vue
@@ -1,37 +1,50 @@
 <template>
   <div style="
     width: 99.9%;
-    height: 83vh;
+    height: 885px;
     border: 1px red solid;
     display: flex;
     justify-content: space-around;
-    flex-wrap: wrap;"
-  >
-    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
-
+    flex-wrap: wrap;">
+    <div class="main">
+      <echart-one :chartsId="chartOneId"/>
     </div>
-    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
-
+    <div class="main">
+      <echart-two :chartsId="chartTwoId"/>
     </div>
-    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
-
+    <div class="main">
+      <echart-three :chartsId="chartThreeId"/>
     </div>
-    <div style="width: 49%; height: 41vh; border: 1px yellowgreen solid">
-
+    <div class="main">
+      <echart-four :chartsId="chartFourId"/>
     </div>
   </div>
-
 </template>
 
 <script>
 import {defineComponent, reactive, toRefs} from 'vue'
+import echarts1 from "./echarts1";
+import echarts2 from "./echarts2";
+import echarts3 from "./echarts3";
+import echarts4 from "./echarts4";
 
 export default defineComponent({
   name: "homeMain",
 
+  components: {
+    'echart-one': echarts1,
+    'echart-two': echarts2,
+    'echart-three': echarts3,
+    'echart-four': echarts4,
+  },
+
   setup() {
     const state = reactive({
       count: 1,
+      chartOneId: 'chartOne',
+      chartTwoId: 'chartTwo',
+      chartThreeId: 'chartThree',
+      chartFourId: 'chartFour',
     })
 
     return {
@@ -42,5 +55,9 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+.main {
+  width: 49%;
+  height: 440px;
+  /*border: 1px yellowgreen solid;*/
+}
 </style>
