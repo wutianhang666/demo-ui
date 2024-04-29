a3658c976afe4844e249bc86fec388387227680a
wutianhang <mr_wu163@163.com>
2024-04-29 23:08:53 +0800

    下拉事件

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -13,12 +13,12 @@
           :key="index"
           :to="{path: '/sys/home'}"
       >
-        {{item.meta.title}}
+        {{ item.meta.title }}
       </el-breadcrumb-item>
     </el-breadcrumb>
 
     <!--     下拉菜单     -->
-    <el-dropdown>
+    <el-dropdown @command="handleCommand">
       <span class="el-dropdown-link">
         <!--头像-->
         <el-avatar :size="32" :src="'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'"/>
@@ -28,8 +28,8 @@
       </span>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item>姓名</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <el-dropdown-item command="a">个人中心</el-dropdown-item>
+          <el-dropdown-item command="b">退出</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
@@ -56,7 +56,11 @@ export default defineComponent({
       count: 1,
     });
 
-    const methods = {}
+    const methods = {
+      handleCommand: (command) => {
+        alert(command)
+      },
+    }
     return {
       ...toRefs(state),
       flag,
